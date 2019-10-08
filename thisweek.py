c4e68697457ce04564f49a773aff46a72c2eb82d
100134315 <100134315@lubbockisd.net>
2019-10-08 15:18:53 -0500

    first commit of 10/8

--- thisweek.py
+++ thisweek.py
@@ -41,3 +41,47 @@ end12 = "r"
 
 print(end1 + end2 + end3 + end4 + end5 + end6)
 print(end7 + end8 + end9 + end10 + end11 + end12)
+
+# But wait! There's more:
+formatter = "%r %r %r %r"
+print(formatter % (1, 2, 3, 4))
+print(formatter % ("one", "two", "three", "four"))
+print(formatter % (True, False, False, True))
+print(formatter % (formatter, formatter, formatter, formatter))
+
+# why do i use %r instead of %s it the above example?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
