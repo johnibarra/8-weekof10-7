2af77f3576dc422d4d6b573bd3e95afb5a417da7
100134315 <100134315@lubbockisd.net>
2019-10-08 15:30:40 -0500

    first commit of 10/8

--- thisweek.py
+++ thisweek.py
@@ -51,6 +51,18 @@ print(formatter % (formatter, formatter, formatter, formatter))
 
 # why do i use %r instead of %s it the above example?
 
+days = "mon tue wed thu fri sat sun"
+months = "Jan\nFeb\nMar\naApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months ", months)
+
+print("""
+There's something going on here.
+with the three double-qoutes.
+we'll be able to type as much as we like.
+even 4 lines if we want.
+""")
 
 
 
